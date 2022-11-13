88354691c6c77ae5351cc7bb70fef88d04382381
Jose Martinez <44812411+JoseM111@users.noreply.github.com>
2022-11-12 22:14:21 -0500

    EPIC0001: validated inputs & reurning data in actions( ) [ Oct 11th ]
    Took 2 hours 7 minutes

--- remix-course/app/components/new_notes/NewNotes.tsx
+++ remix-course/app/components/new_notes/NewNotes.tsx
@@ -1,15 +1,34 @@
 // NewNotes.tsx
+import { Form, useActionData, useTransition as useNavigation } from '@remix-run/react';
 import newNotesStyles from 'app/components/new_notes/NewNotesStyles.css';
-import type { ReactElement } from "react"
+import type { ReactElement } from 'react';
 
 function NewNotes(): ReactElement {
+  const navigation = useNavigation();
+  const isSubmitting: boolean = navigation.state === 'submitting';
+  const buttonLabelState: string = isSubmitting ? 'Adding...' : 'Add Note';
+  // Returns the JSON parsed data from the current route's action data
+  const actionNotesData = useActionData();
+
+  const actionDataStyles = {
+    fontSize: '2rem',
+    color: 'dodgerblue',
+    fontFamily: 'agave Nerd Font Mono',
+  };
+
+  const isTitleToShort = actionNotesData?.message && <p style={actionDataStyles}>{actionNotesData.message}</p>;
+
   return (
     // side-note: `method` is a default HTML attribute for the <form> element
     // this behavior is the browser default in the past via `event.preventDefault()`.
     // by default, upon submitting the form, the browser will send an HTTP request to the server
     // this component by default will send the post request to the route `/notes` since it is
     // being used in the notes.tsx file/page
-    <form method="post" id="note-form">
+    <Form method="post" id="note-form">
+      {/*If the title is at least 5 characters long, the
+       note will be added to the list, but if not than the
+       `actionNotesData.message` will render in the notes page*/}
+      {isTitleToShort}
       <p>
         <label htmlFor="title">Title</label>
         <input type="text" id="title" name="title" required />
@@ -19,9 +38,9 @@ function NewNotes(): ReactElement {
         <textarea id="content" name="content" rows={5} required />
       </p>
       <div className="form-actions">
-        <button>Add Note</button>
+        <button disabled={isSubmitting}>{buttonLabelState}</button>
       </div>
-    </form>
+    </Form>
   );
 }
 
--- remix-course/app/routes/notes.tsx
+++ remix-course/app/routes/notes.tsx
@@ -5,22 +5,22 @@ import type { ReactElement } from 'react';
 import NewNotes, { links as newNoteLinks } from '~/components/new_notes/NewNotes';
 import NotesList, { links as notesListLinks } from '~/components/notes_list/NotesList';
 import { getStoredNotes, storeNotes } from '~/data/notes';
-import type { ActionParams } from '~/types/types';
+import type { ActionParams, NotesType } from '~/types/types';
 
 function NotesPage(): ReactElement {
-  const loaderNotes = useLoaderData();
-
+  // Returns the JSON parsed data from the current route's loader data
+  const loaderNotesData = useLoaderData();
+  
   return (
     <main>
       {/*new-note component*/}
       <NewNotes />
-      <NotesList notes={loaderNotes} />
+      <NotesList notes={loaderNotesData} />
     </main>
   );
 }
 
 export default NotesPage;
-
 // ########################################################
 
 export async function loader() {
@@ -33,19 +33,31 @@ export async function loader() {
 // execute & store this function in the server.
 export async function action({ request }: ActionParams) {
   const formData = await request.formData();
-  const noteData = Object.fromEntries(formData);
+  const noteData = Object.fromEntries(formData) as NotesType;
 
-  // get the existing notes
-  const existingNotes = await getStoredNotes();
-  // add a new prototype property to the noteData object
-  noteData.id = new Date().toISOString();
+  // validation of title length
+  if (noteData.title.trim().length >= 5) {
+    // get the existing notes
+    const existingNotes = await getStoredNotes();
+    noteData.id = new Date().toISOString();
 
-  // add the new note to the existing notes
-  const updatedNotes = existingNotes.concat(noteData);
-  await storeNotes(updatedNotes);
+    // add the new note to the existing notes
+    const updatedNotes = existingNotes.concat(noteData);
 
-  // if all goes well, redirect to the notes page
-  return redirect('/notes');
+    // Since the reload is so fast we want t set a time-out
+    await new Promise<void>((resolve) => {
+      setTimeout(() => resolve(), 2000);
+    });
+
+    await storeNotes(updatedNotes);
+    // if all goes well, redirect to the notes page
+    return redirect('/notes');
+  }
+
+  // if the length is less than five characters, return an error message
+  return {
+    message: 'Title must be at least 5 characters long',
+  };
 }
 
 // links function for NewNotesStyles
